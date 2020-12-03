986dec067dbba8542dae408fb4c3c36ebc1c60f7
John Darryl <johndarrylpelingo@gmail.com>
2020-12-03 03:54:53 +0100

    chore: find out branch name

--- build/index.js
+++ build/index.js
@@ -1,4 +1,18 @@
 'use strict'
-const fullName = 'John Darryl Pelingo'
-console.log(`Hello, ${fullName}`)
+Object.defineProperty(exports, '__esModule', { value: true })
+exports.run = void 0
+const core_1 = require('@actions/core')
+const getBranchName = () => process.env.GITHUB_HEAD_REF
+const run = async () => {
+  const branchName = getBranchName()
+  try {
+    console.log(branchName)
+  } catch (error) {
+    core_1.setFailed(error)
+  }
+}
+exports.run = run
+if (process.env['NODE_ENV'] !== 'test') {
+  exports.run()
+}
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
-const fullName = 'John Darryl Pelingo'
+import { setFailed } from '@actions/core'
 
-console.log(`Hello, ${fullName}`)
+const getBranchName = (): string => process.env.GITHUB_HEAD_REF as string
+
+export const run = async (): Promise<void> => {
+  const branchName = getBranchName()
+
+  try {
+    console.log(branchName)
+  } catch (error) {
+    setFailed(error)
+  }
+}
+
+if (process.env['NODE_ENV'] !== 'test') {
+  run()
+}
